779a0fb451ea2998f6539cd73390e02093c138fa
satvimoral05 <satvimoral@gmail.com>
2026-08-23 15:52:25 +0530

    Implement basic threading example in ThreadDemo

---
+++ LAB-13/ThreadDemo.java
@@ -0,0 +1,26 @@
+class MyThread extends Thread {
+
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("Thread using Thread class: " + i);
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class ThreadDemo {
+
+    public static void main(String[] args) {
+
+        MyThread t1 = new MyThread();
+
+        t1.start();
+    }
+}
