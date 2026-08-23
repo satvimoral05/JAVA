77201c9d7c5fa0abcd9a7173b9504e9eb2584c22
satvimoral05 <satvimoral@gmail.com>
2026-08-23 15:05:49 +0530

    Add temperature conversion from Fahrenheit to Celsius

---
+++ LAB-3/Temp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = sc.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println(
+            "Fahrenheit: " + fahrenheit + " = Celsius: " + celsius
+        );
+
+        sc.close();
+    }
+}
