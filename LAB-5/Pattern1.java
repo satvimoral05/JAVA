61abaef87e9b36f72d67274b12cadfe519c849f5
satvimoral05 <satvimoral@gmail.com>
2026-08-23 15:23:46 +0530

    Add pattern printing functionality in Pattern1.java
    Implement pattern printing based on user input string.

---
+++ LAB-5/Pattern1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(str.charAt(j));
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
